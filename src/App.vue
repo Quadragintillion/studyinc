3afe0f148f5d94575d195ce42b836f25cea8ba22
Dragin <dragin@dragin.xyz>
2026-02-25 16:06:14 -0600

    Preload icons, move loading screen to BasePage.vue

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,11 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
 import ToolsPage from './components/pages/tools/ToolsPage.vue'
 import Sidebar from './components/sidebar/Sidebar.vue'
-import LoadingIcon from './components/misc/LoadingIcon.vue'
-
-const loading = ref(true);
-
-onMounted(() => {
-  if (document.readyState === 'complete') {
-    loading.value = false
-  } else {
-    window.addEventListener('load', () => { loading.value = false; }, { once: true })
-  }
-})
 </script>
 
 <template>
-  <Transition name="fade">
-    <div v-if="loading" class="fixed inset-0 z-50 flex items-center justify-center styled-background">
-      <LoadingIcon />
-    </div>
-  </Transition>
   <div class="flex m-3">
     <Sidebar class="mr-3" />
     <ToolsPage />
   </div>
 </template>
-
-<style scoped>
-.fade-leave-active {
-  transition: opacity 300ms ease-out;
-}
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
--- src/components/pages/BasePage.vue
+++ src/components/pages/BasePage.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import LoadingIcon from '../misc/LoadingIcon.vue'
 
+const loading = ref(true)
+
+onMounted(() => {
+  if (document.readyState === 'complete') {
+    loading.value = false
+  } else {
+    window.addEventListener('load', () => { loading.value = false }, { once: true })
+  }
+})
 </script>
 
 <template>
-  <div id="page" class="styled-background full-height w-full rounded-3xl">
+  <div id="page" class="styled-background full-height w-full rounded-3xl relative overflow-hidden">
+    <Transition name="fade">
+      <div v-if="loading" class="absolute inset-0 z-50 flex items-center justify-center styled-background rounded-3xl">
+        <LoadingIcon />
+      </div>
+    </Transition>
     <slot />
   </div>
 </template>
+
+<style scoped>
+.fade-leave-active {
+  transition: opacity 300ms ease-out;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
