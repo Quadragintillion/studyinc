cd7ed1dd90d0c361fe5d8fc150dd64259919d7f1
Dragin <dragin@dragin.xyz>
2026-03-06 21:57:21 -0600

    Close tool when tool button clicked

--- src/stores/page.ts
+++ src/stores/page.ts
@@ -1,10 +1,12 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { useToolStore } from "./tools";
 
 export const usePageStore = defineStore('page', () => {
   const page = ref('Study Tools') // Can be any of the names on the sidebar
 
   function changePage(newPage: string) {
+    if (newPage == 'Study Tools') useToolStore().openTool(null)
     page.value = newPage
   }
 
